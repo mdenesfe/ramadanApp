8d5b59e423c9796b4cf3930cdc3c47b1fd3b2a4b
Enes Demir <91777967+mdenesfe@users.noreply.github.com>
2024-03-05 18:41:20 +0300

    ramadanApp

---
+++ dist/manifest_y3MdlcSw.mjs
@@ -0,0 +1,80 @@
+import '@astrojs/internal-helpers/path';
+import 'cookie';
+import 'kleur/colors';
+import 'string-width';
+import 'html-escaper';
+import 'clsx';
+import './chunks/astro_cqkYRnFf.mjs';
+import { compile } from 'path-to-regexp';
+
+if (typeof process !== "undefined") {
+  let proc = process;
+  if ("argv" in proc && Array.isArray(proc.argv)) {
+    if (proc.argv.includes("--verbose")) ; else if (proc.argv.includes("--silent")) ; else ;
+  }
+}
+
+function getRouteGenerator(segments, addTrailingSlash) {
+  const template = segments.map((segment) => {
+    return "/" + segment.map((part) => {
+      if (part.spread) {
+        return `:${part.content.slice(3)}(.*)?`;
+      } else if (part.dynamic) {
+        return `:${part.content}`;
+      } else {
+        return part.content.normalize().replace(/\?/g, "%3F").replace(/#/g, "%23").replace(/%5B/g, "[").replace(/%5D/g, "]").replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      }
+    }).join("");
+  }).join("");
+  let trailing = "";
+  if (addTrailingSlash === "always" && segments.length) {
+    trailing = "/";
+  }
+  const toPath = compile(template + trailing);
+  return toPath;
+}
+
+function deserializeRouteData(rawRouteData) {
+  return {
+    route: rawRouteData.route,
+    type: rawRouteData.type,
+    pattern: new RegExp(rawRouteData.pattern),
+    params: rawRouteData.params,
+    component: rawRouteData.component,
+    generate: getRouteGenerator(rawRouteData.segments, rawRouteData._meta.trailingSlash),
+    pathname: rawRouteData.pathname || void 0,
+    segments: rawRouteData.segments,
+    prerender: rawRouteData.prerender,
+    redirect: rawRouteData.redirect,
+    redirectRoute: rawRouteData.redirectRoute ? deserializeRouteData(rawRouteData.redirectRoute) : void 0,
+    fallbackRoutes: rawRouteData.fallbackRoutes.map((fallback) => {
+      return deserializeRouteData(fallback);
+    })
+  };
+}
+
+function deserializeManifest(serializedManifest) {
+  const routes = [];
+  for (const serializedRoute of serializedManifest.routes) {
+    routes.push({
+      ...serializedRoute,
+      routeData: deserializeRouteData(serializedRoute.routeData)
+    });
+    const route = serializedRoute;
+    route.routeData = deserializeRouteData(serializedRoute.routeData);
+  }
+  const assets = new Set(serializedManifest.assets);
+  const componentMetadata = new Map(serializedManifest.componentMetadata);
+  const clientDirectives = new Map(serializedManifest.clientDirectives);
+  return {
+    ...serializedManifest,
+    assets,
+    componentMetadata,
+    clientDirectives,
+    routes
+  };
+}
+
+const manifest = deserializeManifest({"adapterName":"","routes":[{"file":"","links":[],"scripts":[{"type":"external","value":"/_astro/hoisted.1XEFCPvF.js"}],"styles":[{"type":"external","src":"/_astro/index.HpaBiqn-.css"}],"routeData":{"route":"/","type":"page","pattern":"^\\/$","segments":[],"params":[],"component":"src/pages/index.astro","pathname":"/","prerender":false,"fallbackRoutes":[],"_meta":{"trailingSlash":"ignore"}}}],"site":"https://lexingtonthemes.com","base":"/","trailingSlash":"ignore","compressHTML":true,"componentMetadata":[["/Users/mdenesfe/Downloads/prima-persona-main/src/pages/index.astro",{"propagation":"none","containsHead":true}]],"renderers":[],"clientDirectives":[["idle","(()=>{var i=t=>{let e=async()=>{await(await t())()};\"requestIdleCallback\"in window?window.requestIdleCallback(e):setTimeout(e,200)};(self.Astro||(self.Astro={})).idle=i;window.dispatchEvent(new Event(\"astro:idle\"));})();"],["load","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).load=e;window.dispatchEvent(new Event(\"astro:load\"));})();"],["media","(()=>{var s=(i,t)=>{let a=async()=>{await(await i())()};if(t.value){let e=matchMedia(t.value);e.matches?a():e.addEventListener(\"change\",a,{once:!0})}};(self.Astro||(self.Astro={})).media=s;window.dispatchEvent(new Event(\"astro:media\"));})();"],["only","(()=>{var e=async t=>{await(await t())()};(self.Astro||(self.Astro={})).only=e;window.dispatchEvent(new Event(\"astro:only\"));})();"],["visible","(()=>{var r=(i,c,s)=>{let n=async()=>{await(await i())()},t=new IntersectionObserver(e=>{for(let o of e)if(o.isIntersecting){t.disconnect(),n();break}});for(let e of s.children)t.observe(e)};(self.Astro||(self.Astro={})).visible=r;window.dispatchEvent(new Event(\"astro:visible\"));})();"]],"entryModules":{"\u0000@astro-page:src/pages/index@_@astro":"pages/index.astro.mjs","\u0000@astro-renderers":"renderers.mjs","\u0000empty-middleware":"_empty-middleware.mjs","/src/pages/index.astro":"chunks/pages/index_4zpGz-Yy.mjs","\u0000@astrojs-manifest":"manifest_y3MdlcSw.mjs","/astro/hoisted.js?q=0":"_astro/hoisted.1XEFCPvF.js","astro:scripts/before-hydration.js":""},"assets":[]});
+
+export { manifest };
---
+++ tailwind.config.cjs
@@ -0,0 +1,117 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontSize: {
+      xs: [
+        "0.75rem",
+        {
+          lineHeight: "1rem",
+        },
+      ],
+      sm: [
+        "0.875rem",
+        {
+          lineHeight: "1.5rem",
+        },
+      ],
+      base: [
+        "1rem",
+        {
+          lineHeight: "1.75rem",
+        },
+      ],
+      lg: [
+        "1.125rem",
+        {
+          lineHeight: "2rem",
+        },
+      ],
+      xl: [
+        "1.25rem",
+        {
+          lineHeight: "2rem",
+        },
+      ],
+      "2xl": [
+        "1.5rem",
+        {
+          lineHeight: "2rem",
+        },
+      ],
+      "3xl": [
+        "2rem",
+        {
+          lineHeight: "2.5rem",
+        },
+      ],
+      "4xl": [
+        "2.5rem",
+        {
+          lineHeight: "3.5rem",
+        },
+      ],
+      "5xl": [
+        "3rem",
+        {
+          lineHeight: "3.5rem",
+        },
+      ],
+      "6xl": [
+        "3.75rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "7xl": [
+        "4.5rem",
+        {
+          lineHeight: "1.1",
+        },
+      ],
+      "8xl": [
+        "6rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "9xl": [
+        "8rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+    },
+    extend: {
+      
+      boxShadow: {
+        thick: "0px 7px 32px rgb(0 0 0 / 35%);",
+      },
+      colors: {
+        // Gray
+        "primary": "#101010",
+  "secondary": "#1a1a1a",
+  "tertiary": "#262626",
+        // Purple
+        //primary:"#080118",
+        //secondary:"#140d23",
+        //tertiary:"#1d1333",
+        white:"#ececec",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "3rem",
+        "6xl": "5rem",
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+}
