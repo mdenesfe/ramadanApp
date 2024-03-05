4574b05a25bed9be9ec8b6a211ea62c249effa59
Enes Demir <91777967+mdenesfe@users.noreply.github.com>
2024-03-05 23:16:25 +0300

    Update and fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     fontSize: {
       xs: [
