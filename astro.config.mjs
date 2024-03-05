b2b21ecf0faa8f0122dbd5ce70024bcd7ef7d7d5
Enes Demir <91777967+mdenesfe@users.noreply.github.com>
2024-03-06 01:36:32 +0300

    working without internet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,3 @@
-// astro.config.mjs
-
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 const CACHE_NAME = 'ramazan-app-cache-v1';
-
 const urlsToCache = [
   '/',
   '/src/pages/index.astro',
@@ -10,6 +9,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -23,8 +23,36 @@ self.addEventListener('fetch', function(event) {
           return response;
         }
 
-        return fetch(event.request);
-      }
-    )
+        return fetch(event.request).then(function(response) {
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
