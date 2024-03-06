3777eb28097835a73c400e623905c07b493071f5
Enes Demir <91777967+mdenesfe@users.noreply.github.com>
2024-03-06 12:46:24 +0300

    sitemapü

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://ramadan-app-bice.vercel.app/',
+  site: 'https://ramazan.app',
   integrations: [tailwind(), sitemap()],
   vite: {
     optimizeDeps: {
